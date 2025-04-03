e82dc4bfe6dbba81122fc117ea58bad8e8946d7e
Tomi1460 <tomassaldiviawalter@gmail.com>
2025-04-03 18:39:15 -0300

    Add files via upload

---
+++ Actividad1.py
@@ -0,0 +1,19 @@
+clasificación = int(input("Ingrese su nota: "))  
+
+if clasificación >= 90:
+    
+    print("Excelente trabajo")
+    
+elif clasificación >= 80:
+    
+    print("Muy buen trabajo")
+    
+elif clasificación >= 70:
+    
+    print("Buen trabajo")
+    
+elif clasificación < 70:
+    print("Malisimo")
+    
+    
+    
